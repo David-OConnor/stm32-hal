cc3fb2bd9a1f72ef2b263d61ffbd6f464ef6e61d
David O'Connor <david.alan.oconnor@gmail.com>
2021-10-07 16:15:18 -0400

    More PAC change fix

--- src/lib.rs
+++ src/lib.rs
@@ -531,7 +531,7 @@ pub fn debug_workaround() {
         let dbgmcu = unsafe { &(*pac::DBGMCU::ptr()) };
 
         cfg_if::cfg_if! {
-            if #[cfg(all(feature = "h7", not(feature = "h747cm7")))] {
+            if #[cfg(all(feature = "h7", not(any(feature = "h747cm4", feature = "h747cm7"))))] {
                 dbgmcu.cr.modify(|_, w| w.dbgsleep_d1().set_bit());
                 dbgmcu.cr.modify(|_, w| w.dbgstop_d1().set_bit());
                 dbgmcu.cr.modify(|_, w| w.dbgstby_d1().set_bit());
