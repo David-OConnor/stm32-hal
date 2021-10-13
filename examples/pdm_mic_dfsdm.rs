3a42bbcdad3b4362100d8f8e57eb987ffdf164d2
David O'Connor <david.alan.oconnor@gmail.com>
2021-10-12 20:42:59 -0400

    Removed old placeholder pdm example

--- examples/pdm_mic_dfsdm.rs
+++
@@ -1,5 +0,0 @@
-//! This example demonstrates reading input using a PDM microphone using the DFSDM
-//! peripheral. It uses a circular DMA buffer to continuously read and process from the microphone,
-//! and outputs to the DAC using a second circular buffer.
-//!
-//! This example uses [RTIC](https://rtic.rs/0.5/book/en/) for its structure.
