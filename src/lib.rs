c4e1405e27f30790f7c82cc46c3a3452e928bcd6
u296 <u296b3g@gmail.com>
2022-10-08 23:45:11 +0200

    correct usb connectivity type for l4x5

--- src/lib.rs
+++ src/lib.rs
@@ -507,6 +507,7 @@ cfg_if::cfg_if! {
                 feature = "l4x2",
                 feature = "l412",
                 feature = "l4x3",
+                feature = "l4x5",
                 feature = "l5",
                 feature = "g4",
                 feature = "wb",
@@ -517,7 +518,7 @@ cfg_if::cfg_if! {
     } else if #[cfg(all(
     // todo: I think only H7 has hs (high-speed), while F4 and L4 have FS only.
         any(feature = "usbotg_fs", feature = "usbotg_hs"),
-        any(feature = "f4", feature = "l4x5", feature = "l4x6", feature = "h7")
+        any(feature = "f4", feature = "l4x6", feature = "h7")
     ))] {
         pub mod usb_otg; // todo: A way to export it as `usb`?
     }
--- src/usb.rs
+++ src/usb.rs
@@ -3,9 +3,20 @@
 //!
 //! Requires the `usb` feature.
 //!
-//! Used on F303, L4x2, L4x3, L5, G0, and G4. F4, L4x5, L4x6 and H7 use the `usb_otg` module.
+//! Used on F303, L4x2, L4x3, L4x5, L5, G0, and G4. F4, L4x6 and H7 use the `usb_otg` module.
 //! For G0 series, USB is only available on G0B0, G0B1, G0C1, which the PAC doesn't yet differentiate,
 //! and this library doesn't yet support.
+
+/* 
+ Small caveat, the pac for the l4x5 exposes a USB peripheral instead
+ of a OTG peripheral, even though this chart
+ https://www.st.com/en/microcontrollers-microprocessors/stm32l4-series.html
+ shows that the stm32l475 has OTG.
+
+ Further inspection shows that the generated pac for the l4x5 has a USB peripheral
+ while the l4r5 has an OTG, but this crate doesn't currently seem to support the 
+ l4r5 variant.
+*/
 
 use crate::{pac, util::rcc_en_reset};
 
