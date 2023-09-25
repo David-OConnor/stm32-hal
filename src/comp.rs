209339b7abedf3e396dc38fb5e56a980fea121b9
boondockenergy <matt@boondocker.io>
2023-09-25 08:34:27 -0700

    Refactoring comp to work with G474

--- src/comp.rs
+++ src/comp.rs
@@ -7,10 +7,27 @@
 //! - Moving Peripheral into Struct (pac needs to change)
 //! - Add Configuration Defaults
 //! - Interrupts?
+
+use core::marker::PhantomData;
+
 use crate::pac;
+
+use paste::paste;
+
+use cfg_if::cfg_if;
+use crate::pac::comp::{
+    C1CSR,
+    C2CSR,
+    C3CSR,
+    C4CSR,
+    C5CSR,
+    C6CSR,
+    C7CSR
+};
 
 // Config enums
 /// Comparator power mode
+#[cfg(any(feature = "l4"))]
 pub enum PowerMode {
     /// High speed/full power (Lowest propagation delay).
     HighSpeed = 0x00000000,
@@ -21,6 +38,16 @@ pub enum PowerMode {
 }
 
 /// Comparator input plus (Non-inverting Input)
+#[cfg(any(feature = "g4"))]
+#[derive(Clone,Copy)]
+#[repr(u8)]
+// STM32G4 reference manual section 24.3.2 table 196
+pub enum NonInvertingInput {
+    Io1 = 0b0,
+    Io2 = 0b1
+}
+
+#[cfg(any(feature = "l4"))]
 pub enum NonInvertingInput {
     /// From the first GPIO pin connected to the comparator.
     ///
@@ -39,8 +66,24 @@ pub enum NonInvertingInput {
     Io3 = 0x00000100,
 }
 
-// TODO Values are based on SCALEN (0x800000) and BRGEN (0x400000) check for other MCU.
 /// Comparator input minus (Inverted Input)
+#[cfg(any(feature = "g4"))]
+#[derive(Clone,Copy)]
+#[repr(u8)]
+// STM32G4 reference manual section 24.3.2 table 197
+pub enum InvertingInput {
+    OneQuarterVref = 0b000,
+    OneHalfVref = 0b001,
+    ThreeQuarterVref = 0b010,
+    Vref = 0b011,
+    Dac1 = 0b100,
+    Dac2 = 0b101,
+    Io1 = 0b110,
+    Io2 = 0b111
+}
+
+#[cfg(any(feature = "l4"))]
+// TODO Values are based on SCALEN (0x800000) and BRGEN (0x400000) check for other MCU.
 pub enum InvertingInput {
     /// 1/4 of Vref
     OneQuarterVref = 0x00c00000,
@@ -65,6 +108,20 @@ pub enum InvertingInput {
 }
 
 /// Comparator hysterisis
+#[cfg(any(feature = "g4"))]
+#[derive(Clone,Copy)]
+pub enum Hysterisis {
+    None = 0b000,
+    TenMilliVolt = 0b001,
+    TwentyMilliVolt = 0b010,
+    ThirtyMilliVolt = 0b011,
+    FourtyMilliVolt = 0b100,
+    FiftyMilliVolt = 0b101,
+    SixtyMilliVolt = 0b110,
+    SeventyMilliVolt = 0b111,
+}
+
+#[cfg(any(feature = "l4"))]
 pub enum Hysterisis {
     /// No Hysterisis.
     NoHysterisis = 0x00000000,
@@ -87,6 +144,15 @@ pub enum Hysterisis {
 /// The comparator output will be high (1) when [NonInvertingInput] has lower
 /// voltage than [InvertingInput]. The comparator output will be low (0) when
 /// [NonInvertingInput] has higher voltage than [InvertingInput].
+
+#[cfg(any(feature = "g4"))]
+#[derive(Clone,Copy)]
+pub enum OutputPolarity {
+    NotInverted = 0b0,
+    Inverted = 0b1
+}
+
+#[cfg(any(feature = "l4"))]
 pub enum OutputPolarity {
     /// Comparator output will not be inverted.
     NotInverted = 0x00000000,
@@ -103,16 +169,30 @@ pub enum BlankingSource {
 }
 
 /// Comparator devices avaiable.
+#[cfg(any(feature = "g4"))]
 pub enum CompDevice {
-    /// Comparator number 1 (COMP1).
     One,
-    /// Comparator number 2 (COMP2).
-    #[cfg(not(any(feature = "l412")))]
     Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven
 }
 
 // Structs
 /// Initial configuration data for the comparator peripheral.
+
+#[cfg(any(feature = "g4"))]
+#[derive(Clone,Copy)]
+pub struct CompConfig {
+    pub inpsel: NonInvertingInput,
+    pub inmsel: InvertingInput,
+    pub hyst: Hysterisis,
+    pub polarity: OutputPolarity
+}
+
+#[cfg(any(feature = "l4"))]
 pub struct CompConfig {
     /// Comparator power mode.
     pub pwrmode: PowerMode,
@@ -128,173 +208,99 @@ pub struct CompConfig {
     // pub blanking: BlankingSource,
 }
 
-/// Macro to write bits to the register
-macro_rules! set_bit {
-    ($comp:ident, $value:expr) => {
-        unsafe {
-            let regs = &(*pac::COMP::ptr()).$comp;
-            regs.modify(|r, w| {
-                let current_bits = r.bits();
-                let output_bits = current_bits | $value;
-                w.bits(output_bits)
-            })
-        }
-    };
-}
-
-/// Macro to clear bits in the register
-macro_rules! clear_bit {
-    ($comp:ident, $value:expr) => {
-        unsafe {
-            let regs = &(*pac::COMP::ptr()).$comp;
-            let current_bits = regs.read().bits();
-            let output_bits = current_bits & !$value;
-            regs.modify(|_, w| w.bits(output_bits))
-        }
-    };
-}
-
-/// Macro to read bits in the register
-macro_rules! read_bit {
-    ($comp:ident, $value:expr) => {
-        unsafe {
-            let regs = &(*pac::COMP::ptr()).$comp;
-            regs.read().bits() & $value
-        }
-    };
-}
-
-/// Macro to modify the register
-macro_rules! modify_bit {
-    ($comp:ident, $value:expr) => {
-        unsafe {
-            let regs = &(*pac::COMP::ptr()).$comp;
-            regs.write(|w| w.bits($value))
-        }
-    };
-}
-
 /// Represents an Analog Comparator peripheral.
-pub struct Comp {
-    /// The comparator device.
-    device: CompDevice,
+pub struct Comp<T> {
+    phantom: PhantomData<T>,
     /// The lock status of the comparator.
     is_locked: bool,
 }
 
-impl Comp {
-    /// Initialize the comparator peripheral. This will writes the configuration
-    /// according to `cfg`.
-    pub fn new(device: CompDevice, cfg: CompConfig) -> Self {
-        let result = Self {
-            device,
-            is_locked: false,
-        };
+// Macro to implement a comparator using generics
+// This will create `new_compX` methods to instantiate a new comparator
+// and provide a csr() method to access the register scoped to this comparator
+macro_rules! make_comp {
+    ($csr_type:ident, $csr_reg:ident, $comp:ident) => {
+        impl Comp<$csr_type>
+        {
+            paste! {
+                pub fn [<new_ $comp>]() -> Self {
+                    unsafe {
+                        Self {
+                            phantom: PhantomData,
+                            is_locked: false
+                        }
+                    }
+                }
+            }
 
-        let config = cfg.hyst as u32
-            | cfg.inmsel as u32
-            | cfg.inpsel as u32
-            | cfg.polarity as u32
-            | cfg.pwrmode as u32;
+            // Get a reference to the CSR from the COMP RegisterBlock for this comparator
+            pub fn csr(&self) -> &$csr_type {
+                unsafe { &(*pac::COMP::ptr()).$csr_reg }
+            }
 
-        // Setting enable bit just incase it isn't done during clock setup
-        unsafe {
-            let rcc = &(*pac::RCC::ptr());
-            rcc.apb2enr.write(|w| w.syscfgen().set_bit());
-            rcc.apb2rstr.write(|w| w.syscfgrst().set_bit());
+            pub fn enable(&self) {
+                self.csr().modify(|_,w| w.en().set_bit());
+            }
+
+            pub fn disable(&self) {
+                self.csr().modify(|_,w| w.en().clear_bit());
+            }
+
+            // Sets the inverting input in the CSR
+            pub fn set_inverting_input(&self, input: InvertingInput) {
+                self.csr().modify(|_,w| w.inmsel().variant(input as u8));
+            }
+
+            pub fn set_non_inverting_input(&self, input: NonInvertingInput) {
+                self.csr().modify(|_,w| w.inpsel().variant(input as u8 != 0));
+            }
+
+            pub fn set_polarity(&self, polarity: OutputPolarity) {
+                self.csr().modify(|_,w| w.pol().variant((polarity as u8) != 0));
+            }
+
+            pub fn set_hysterisis(&self, hyst: Hysterisis) {
+                self.csr().modify(|_,w| w.hyst().variant((hyst as u8)));
+            }
+
+            pub fn set_blanking_source(&self, source: u8) {
+                self.csr().modify(|_,w| w.blanksel().variant(source));
+            }
+
+            /// Locks the comparator.
+            ///
+            /// This locks the comparator registers making it only read-only.
+            ///
+            /// **Note:** The lock also applies to the lock bit itself. Therefore,
+            /// the comparator register/configuration **cannot** be changed until
+            /// a hardware reset.
+            pub fn lock(&mut self) {
+                self.is_locked = true;
+                self.csr().modify(|_,w| w.lock().set_bit());
+            }
+
+            /// Gets the output level of the comparator
+            ///
+            /// The output level depends on the configuration of the comparator.
+            /// If the [polarity](CompConfig::polarity) is [NotInverted](OutputPolarity::NotInverted)
+            /// - It will output high (1) if the non-inverting input is higher than
+            /// the output of inverting input.
+            /// - It will output low (0) if the non-inverting input is lower than
+            /// the output of the inverting input.
+            ///
+            /// The oposite will be out inverted if [polarity](CompConfig::polarity) is
+            /// [Inverted](OutputPolarity::NotInverted).
+            pub fn get_output_level(&self) -> bool {
+                self.csr().read().value().bit()
+            }
         }
-
-        match result.device {
-            CompDevice::One => modify_bit!(comp1_csr, config),
-            #[cfg(not(feature = "l412"))]
-            CompDevice::Two => modify_bit!(comp2_csr, config),
-        }
-        result
-    }
-
-    /// Writes bit/bits to the regiter.
-    fn set_bit(&mut self, value: u32) -> Result<(), ()> {
-        if self.is_locked {
-            return Err(());
-        }
-
-        match self.device {
-            CompDevice::One => set_bit!(comp1_csr, value),
-            #[cfg(not(feature = "l412"))]
-            CompDevice::Two => set_bit!(comp2_csr, value),
-        }
-
-        Ok(())
-    }
-
-    /// Clears bit/bits in the register.
-    ///
-    /// This function will return an Error when the comparator is locked.
-    fn clear_bit(&mut self, value: u32) -> Result<(), ()> {
-        if self.is_locked {
-            return Err(());
-        }
-
-        match self.device {
-            CompDevice::One => clear_bit!(comp1_csr, value),
-            #[cfg(not(feature = "l412"))]
-            CompDevice::Two => clear_bit!(comp2_csr, value),
-        }
-
-        Ok(())
-    }
-
-    /// Read bit/bits in the register.
-    ///
-    /// This function will return an Error when the comparator is locked.
-    fn read_bit(&self, value: u32) -> u32 {
-        match self.device {
-            CompDevice::One => read_bit!(comp1_csr, value),
-            #[cfg(not(feature = "l412"))]
-            CompDevice::Two => read_bit!(comp2_csr, value),
-        }
-    }
-
-    /// Gets the output level of the comparator
-    ///
-    /// The output level depends on the configuration of the comparator.
-    /// If the [polarity](CompConfig::polarity) is [NotInverted](OutputPolarity::NotInverted)
-    /// - It will output high (1) if the non-inverting input is higher than
-    /// the output of inverting input.
-    /// - It will output low (0) if the non-inverting input is lower than
-    /// the output of the inverting input.
-    ///
-    /// The oposite will be out inverted if [polarity](CompConfig::polarity) is
-    /// [Inverted](OutputPolarity::NotInverted).
-    pub fn get_output_level(&self) -> u32 {
-        self.read_bit(0b1 << 30) >> 30
-    }
-
-    /// Starts the comparator.
-    ///
-    /// This function will return an Error when the comparator is locked.
-    pub fn start(&mut self) -> Result<(), ()> {
-        self.set_bit(0b1)
-    }
-
-    /// Stops the comparator.
-    ///
-    /// This function will return an Error when the comparator is locked.
-    pub fn stop(&mut self) -> Result<(), ()> {
-        self.clear_bit(0b1)
-    }
-
-    /// Locks the comparator.
-    ///
-    /// This locks the comparator registers making it only read-only.
-    ///
-    /// **Note:** The lock also applies to the lock bit itself. Therefore,
-    /// the comparator register/configuration **cannot** be changed until
-    /// a hardware reset.
-    ///
-    /// This function will return an Error when the comparator is locked.
-    pub fn lock(&mut self) -> Result<(), ()> {
-        self.is_locked = true;
-        self.set_bit(0x80000000)
     }
 }
+
+make_comp!(C1CSR, c1csr, comp1);
+make_comp!(C2CSR, c2csr, comp2);
+make_comp!(C3CSR, c3csr, comp3);
+make_comp!(C4CSR, c4csr, comp4);
+make_comp!(C5CSR, c5csr, comp5);
+make_comp!(C6CSR, c6csr, comp6);
+make_comp!(C7CSR, c7csr, comp7);
