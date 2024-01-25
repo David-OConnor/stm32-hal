8d1ae2d189b7b70a2ac93ea9e387205e7167df94
David O'Connor <the_alchemist@fastmail.com>
2024-01-25 09:59:47 -0500

    Fixed h753 adc bug

--- src/adc.rs
+++ src/adc.rs
@@ -433,8 +433,11 @@ macro_rules! hal {
                     // ...The software is allowed to write this bit only when ADSTART=0 and JADSTART=0 (which
                     // ensures that no conversion is ongoing)."
                     // todo: On H7, allow disabling boost, either manually, or by checking the clock speed.
-                    #[cfg(feature = "h7")]
+                    #[cfg(any(feature = "h7", not(feature = "h753")))]
                     result.regs.cr.modify(|_, w| w.boost().bits(1));
+
+                    #[cfg(feature = "h753")]
+                    result.regs.cr.modify(|_, w| w.boost().bit(true));
 
                     result.enable();
 
