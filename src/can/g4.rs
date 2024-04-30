803fa9580491489ace076c0e4012122686c9f529
Piet Geursen <piet.geursen@openstar.nz>
2024-04-30 13:20:05 +1200

    Fix message ram location for fdcan2 & 3

--- src/can/g4.rs
+++ src/can/g4.rs
@@ -4,8 +4,18 @@ use crate::{
 };
 use fdcan;
 
+const MESSAGE_RAM_BASE_ADDRESS: u32 = 0x4000_a400;
+
+// The RM is a bit contradictory. Table 3 implies that each FDCAN memory block is 0x400 in size.
+// But section 44.3.3 says each block is 0x350 and that is what actually works.
+const MESSAGE_RAM_SIZE: u32 = 0x350;
+
+const FDCAN1_MESSAGE_RAM_ADDRESS: u32 = MESSAGE_RAM_BASE_ADDRESS;
+const FDCAN2_MESSAGE_RAM_ADDRESS: u32 = FDCAN1_MESSAGE_RAM_ADDRESS + MESSAGE_RAM_SIZE;
+const FDCAN3_MESSAGE_RAM_ADDRESS: u32 = FDCAN2_MESSAGE_RAM_ADDRESS + MESSAGE_RAM_SIZE;
+
 macro_rules! create_cans {
-    ($can:ident, $fdcan:ident, $msg_ram_address:literal ) => {
+    ($can:ident, $fdcan:ident, $msg_ram_address:ident ) => {
         /// Interface to the CAN peripheral.
         pub struct $can {
             pub regs: $fdcan,
@@ -31,13 +41,11 @@ macro_rules! create_cans {
         }
 
         unsafe impl fdcan::message_ram::Instance for $can {
-            // G4 RM, table 3. "Series memory map and peripheral register boundary
-            // addresses". CAN message RAM: 3 listings, 1kb each:
             const MSG_RAM: *mut fdcan::message_ram::RegisterBlock = ($msg_ram_address as *mut _);
         }
     };
 }
 
-create_cans!(Can1, FDCAN1, 0x4000_a400);
-create_cans!(Can2, FDCAN2, 0x4000_a800);
-create_cans!(Can3, FDCAN3, 0x4000_ac00);
+create_cans!(Can1, FDCAN1, FDCAN1_MESSAGE_RAM_ADDRESS);
+create_cans!(Can2, FDCAN2, FDCAN2_MESSAGE_RAM_ADDRESS);
+create_cans!(Can3, FDCAN3, FDCAN3_MESSAGE_RAM_ADDRESS);
